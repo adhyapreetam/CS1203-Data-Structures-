9fb72351442c07b1b912befada070c24f27fd417
adhyapreetam <114255326+adhyapreetam@users.noreply.github.com>
2022-11-07 01:55:19 +0530

    Create QuickSort.c

---
+++ QuickSort.c
@@ -0,0 +1,22 @@
+void quicksort (int *a, int start, int stop){
+  if (!a||start >= stop) return;
+  int p = partition
+    
+  quicksort(a, start, p-1);
+  quicksort(a, p + 1, stop);
+}
+
+int partition(int * a, int start, int stop){
+pivot = a[start]
+  i = start + 1;
+  j = stop;
+  (left of i <= pivot > obj > right of j)
+  
+  while (i<=j)
+    if (a[pivot]{
+      swap(a, i, j);
+      j--;
+      else {i++}
+      swap(a, start, j)
+        return j;
+}
